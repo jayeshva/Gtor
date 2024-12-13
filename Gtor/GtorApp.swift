5d53a7fe2b375e8a0a9d973dab3acf98edc55c0f
jayeshva <jayesh007va@gmail.com>
2024-12-13 14:44:11 +0530

    Initial Commit

---
+++ Gtor/GtorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GtorApp.swift
+//  Gtor
+//
+//  Created by JAYESH V A on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GtorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GtorTests/GtorTests.swift
@@ -0,0 +1,17 @@
+//
+//  GtorTests.swift
+//  GtorTests
+//
+//  Created by JAYESH V A on 13/12/24.
+//
+
+import Testing
+@testable import Gtor
+
+struct GtorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
